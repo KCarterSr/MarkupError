76f0444d14d1e7ac952cc91fd5f91268603ea0f5
Kcarter <KC.Mailroom@CarterWorld.com>
2023-06-20 16:21:59 -0400

    Add project files.

---
+++ MarkupError/Platforms/Windows/StringResourceXamlMarkupExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace MarkupError.WinUI
+{
+ using System;
+
+ public class StringResourceXamlMarkupExtension : IMarkupExtension<string>
+ {
+  private string test;
+
+  public string Test
+  {
+      get => test;
+      set => test = value;
+  }
+
+ object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+ {
+  return (this as IMarkupExtension<string>).ProvideValue(serviceProvider);
+ }
+
+  public string ProvideValue(IServiceProvider serviceProvider)
+  {
+   return "test";
+  }
+ }
+}
